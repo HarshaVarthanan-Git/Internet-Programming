f3c0e7b177376bfe22c4d0097078890cdb8b9366
Harsha Varthanan <163161757+HarshaVarthanan-Git@users.noreply.github.com>
2024-04-12 02:25:54 +0530

    Create Student.java

---
+++ ex9/Student.java
@@ -0,0 +1,36 @@
+public class Student {
+    public String name;
+    public int grade;
+    public String email;
+
+    public Student(String name, int grade, String email) {
+        this.name = name;
+        this.grade = grade;
+        this.email = email;
+    }
+
+    // Getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
